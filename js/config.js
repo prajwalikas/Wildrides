2fc6acec1ff54448fdf074d3e3dac1c1898e6d50
prajwalikas <169014292+prajwalikas@users.noreply.github.com>
2025-05-15 08:17:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_9fBn5xUtI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2v2t5al8shpsvt6p8b83unldq8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
